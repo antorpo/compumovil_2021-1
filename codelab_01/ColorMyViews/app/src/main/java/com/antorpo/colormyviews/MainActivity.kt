cfc8c4eefac9e069823e86872848d57d164746a9
Antonio Gonzalez <antonio.gonzalezr@udea.edu.co>
2021-04-04 19:42:09 -0500

    Creacion de interaccion con los TextView por medio de una funcion
    en el archivo MainActivity.kt, usando la sentencia when de Kotlin que
    es equivalente a el Switch/Case de Java pero con esteroides y con mas
    posibilidades para obtener los casos.
    
    La funcionalidad sera cambiar de color dependiendo del contenido clickeado
    seal el TextView o el Layout Parent (ViewGroup), para poder interactuar con estos
    elementos si o si deben tener un Id para que Android los pueda reconocer.
    
    Agregamos una funcion para crear los listeners de cada uno de los Views y conectarlos
    con la funcion makeColored para poder cambiar su color dependiendo de su id

--- codelab_01/ColorMyViews/app/src/main/java/com/antorpo/colormyviews/MainActivity.kt
+++ codelab_01/ColorMyViews/app/src/main/java/com/antorpo/colormyviews/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.antorpo.colormyviews
-
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    private fun makeColored(view: View) {
+
+        // Sintaxis equivalente al Switch/Case de Java pero con una sintaxis mas amigable
+        // y con mas flexibilidad para poder obtener los casos.
+        when(view.id){
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            else -> view.setBackgroundColor(Color.LTGRAY) // Equivalente al default en Java
+        }
+    }
+
+    private fun setListeners(){
+
+        // Recuperamos una referencia a los Views identificandolos por su Id
+        // Variables de tipo TextView y View
+        val boxOneText = findViewById<TextView>(R.id.box_one_text)
+        val boxTwoText = findViewById<TextView>(R.id.box_two_text)
+        val boxThreeText = findViewById<TextView>(R.id.box_three_text)
+        val boxFourText = findViewById<TextView>(R.id.box_four_text)
+        val boxFiveText = findViewById<TextView>(R.id.box_five_text)
+        val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+
+        // Creamos una lista de Views para asignar de manera mas eficiente los listeners
+        // como los TextView heredan de View podemos crear una lista de tipo <View>
+        val clickableViews: List<View> =
+            listOf(boxOneText, boxTwoText, boxThreeText,
+                boxFourText, boxFiveText, rootConstraintLayout)
+
+        // Agregamos los oyentes que esperan que el evento se produzca para realizar las acciones
+        // cada view va a tener un click listener que va a invocar la funcion makeColored la
+        // cual dependiendo del id del elemento presionado va a cambiar su color
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+
+        }
+
+
 }
