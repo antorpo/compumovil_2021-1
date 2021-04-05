a812c0b92f6cf682982f7ce07eb4d1fdd95f97a8
Antonio Gonzalez <antonio.gonzalezr@udea.edu.co>
2021-04-04 23:41:17 -0500

    Creacion de las instrucciones del app usando dos nuevos
    TextView que esta vez estaran anclados o contectados
    usando un baseline constraint horizontal, falta el constraint
    vertical para evitar que estos dos TextView vayan al principio
    del Layout Parent (vertical = 0).
    
    Al construir los constraint para el TextView info_text podemos
    ver que al moverlo en el Layout Editor ese trae consigo a al TextView
    label_text ya que se encuentra bien conectados.
    
    Creacion de Horizontal Chain (Cadena horizontal) de tres botones.

--- codelab_01/ColorMyViews/app/src/main/java/com/antorpo/colormyviews/MainActivity.kt
+++ codelab_01/ColorMyViews/app/src/main/java/com/antorpo/colormyviews/MainActivity.kt
@@ -3,8 +3,8 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
-import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     // Primer funcion en llamarse
@@ -15,15 +15,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun makeColored(view: View) {
+        val boxOneText = findViewById<TextView>(R.id.box_one_text)
+        val boxTwoText = findViewById<TextView>(R.id.box_two_text)
+        val boxThreeText = findViewById<TextView>(R.id.box_three_text)
 
         // Sintaxis equivalente al Switch/Case de Java pero con una sintaxis mas amigable
         // y con mas flexibilidad para poder obtener los casos.
-        when(view.id){
+        when(view.id) {
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
             R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.red_button -> boxOneText.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> boxTwoText.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> boxThreeText.setBackgroundResource(R.color.my_green)
             else -> view.setBackgroundColor(Color.LTGRAY) // Equivalente al default en Java
         }
     }
@@ -49,18 +55,23 @@ class MainActivity : AppCompatActivity() {
         val boxFourText = findViewById<TextView>(R.id.box_four_text)
         val boxFiveText = findViewById<TextView>(R.id.box_five_text)
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+        val redButton = findViewById<Button>(R.id.red_button)
+        val greenButton = findViewById<Button>(R.id.green_button)
+        val yellowButton = findViewById<Button>(R.id.yellow_button)
+
 
         // Creamos una lista de Views para asignar de manera mas eficiente los listeners
         // como los TextView heredan de View podemos crear una lista de tipo <View>
         val clickableViews: List<View> =
             listOf(boxOneText, boxTwoText, boxThreeText,
-                boxFourText, boxFiveText, rootConstraintLayout)
+                boxFourText, boxFiveText, rootConstraintLayout,
+                redButton, greenButton, yellowButton)
 
         // Agregamos los oyentes que esperan que el evento se produzca para realizar las acciones
         // cada view va a tener un click listener que va a invocar la funcion makeColored la
         // cual dependiendo del id del elemento presionado va a cambiar su color
         for (item in clickableViews) {
-            item.setOnClickListener { makeDrawable(it) }
+            item.setOnClickListener { makeColored(it) }
         }
 
         }
